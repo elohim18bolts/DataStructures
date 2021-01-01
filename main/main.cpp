e450baeec219f7249264a796fbed5744beedb46e
Frank <fdr@fdrcatalog.com>
2021-01-01 17:35:15 -0500

    Adding googletest for TDD

--- main/main.cpp
+++ main/main.cpp
@@ -1,13 +1,13 @@
 #include <iostream>
 #include "linklist/linklist.hpp"
-#include "kbd/kbd.hpp"
+// #include "kbd/kbd.hpp"
 int main()
 {
     int a = 89;
     // ds::Linklist list((void *)&a);
     // std::cout << *(int *)list[1] << "\n";
-    keyboard::Kbd kbd("/dev/input/by-path/platform-i8042-serio-0-event-kbd");
-    kbd.Listen();
+    // keyboard::Kbd kbd("/dev/input/by-path/platform-i8042-serio-0-event-kbd");
+    // kbd.Listen();
 
     return 0;
 }
---
+++ tests/UnitTests.cpp
@@ -0,0 +1,7 @@
+#include "gtest/gtest.h"
+#include "linklist/linklist.hpp"
+
+TEST(dataStructuresTest, linkListTest)
+{
+    ASSERT_EQ(2, 2);
+}
